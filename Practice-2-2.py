12e4b10584815babd288d0e9b425d65189377e4e
sinner1999 <sinner1999@gmail.com>
2021-06-08 09:47:09 +0300

    123

--- Practice-2-2.py
+++ Practice-2-2.py
@@ -1,22 +1,22 @@
 class User:
     count = 0
     def __init__(self, name, login, password):
-        self.__name__ = name
-        self.__login__ = login
-        self.__password__ = password
+        self.__name = name
+        self.__login = login
+        self.__password = password
         self.__class__.count += 1
 
     @property
     def name(self):
-        return self.__name__
+        return self.__name
 
     @name.setter
     def name(self, value):
-        self.__name__ = value
+        self.__name = value
 
     @property
     def login(self):
-        return self.__login__
+        return self.__login
     
     @property
     def pswd(self):
@@ -24,27 +24,29 @@ class User:
 
     @pswd.setter
     def pswd(self, value):
-        self.__password__ = value
+        self.__password = value
 
     def show_info(self):
-        print(f'Name: {self.__name__}\nLogin: {self.__login__}')
+        print(f'Name: {self.__name}\nLogin: {self.__login}')
 
 
 class SuperUser(User):
     count = 0
     def __init__(self, name, login, password, role):
         super().__init__(name, login, password)
-        self.__role__ = role
+        self.__role = role
+        #SuperUser.count += 1
+        #User.count -= 1
 
     @property
     def role(self):
-        return self.__role__
+        return self.__role
 
     @role.setter
     def role(self, value):
-        self.__role__ = value
+        self.__role = value
 
     def show_info(self):
         super().show_info()
-        print(f'Role: {self.__role__}')
+        print(f'Role: {self.__role}')
 
