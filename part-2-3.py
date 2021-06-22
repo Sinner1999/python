752e99a9d26f75a773b8356fbdd31d566f4ada46
sinner1999 <sinner1999@gmail.com>
2021-06-22 08:53:32 +0300

    123

--- part-2-3.py
+++ part-2-3.py
@@ -1,8 +1,20 @@
 def anagram(s1, s2):
     if len(s1) != len(s2):
+        print('oh')
         return False
-    
-    a1 = list(s1).sort()
-    a2 = list(s2).sort()
 
-    return True if a1 == a2 else False
+    p = 0
+    l = len(s1)
+
+    while p < (l):
+        print(p)
+        if s1 == (s2[p:l] + s2[0:p]):
+            print(s2[p:l],s2[0:p])
+            return True
+        else:
+            print(s2[p:l],s2[0:p])
+            p += 1 
+
+    return False
+
+print(anagram('kiragazinka', 'iragazinkak'))
