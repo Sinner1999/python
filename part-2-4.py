73b38f24f14d972c24e27fed752d1e82a4e19b23
sinner1999 <sinner1999@gmail.com>
2021-06-24 17:19:08 +0300

    123

---
+++ part-2-4.py
@@ -0,0 +1,19 @@
+
+
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+
+    def set_data(self, data):
+        self.__data = data
+
+    def get_data(self):
+        return self.__data
+
+    def set_next(self, node):
+        self.__next = node
+
+    def get_next(self):
+        return self.__next
+
