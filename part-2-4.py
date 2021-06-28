fa9d0bb4eae9e92926249ee6f95c89887c31b469
sinner1999 <sinner1999@gmail.com>
2021-06-28 17:19:11 +0300

    123

--- part-2-4.py
+++ part-2-4.py
@@ -31,4 +31,53 @@ class LinkedList:
         self.__head = node
 
     def size(self):
+        current = self.__head
+        count = 0
+        while not current is None:
+            current = current.get_next()
+            count +=1
+        return count
+
+    def search(self, item):
+        current = self.__head
+        while not current is None:
+            if current.get_data() == item:
+                return True
+            current = current.get_next()
+        return False
+
+    def __str__(self):
+        if self.__head is None:
+            return '[]'
+        current = self.__head
+        out = '[' + str(current.get_data())
+        while not current.get_next() is None:
+            current = current.get_next()
+            out += ', ' + str(current.get_data())
+        out += ']'
+        return out
+
+    def remove(self, item):
+        current = self.__head
+        prev = None
+        while not current is None:
+            if current.get_data() == item:
+                if prev is None:
+                    self.__head = current.get_next()
+                else:
+                    prev.set_next(current.get_next())    
+                return True
+            prev = current
+            current = current.get_next()
+        return False 
+
+
+ml = LinkedList()
+ml.add(10)
+ml.add(15)
+ml.add(25)
+ml.add(34)
+ml.add(54)
+ml.add(84)
+ml.add(65)
         
