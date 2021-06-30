4cbec9932fc05c866cc48053e046e18b8537c79e
sinner1999 <sinner1999@gmail.com>
2021-06-30 16:37:57 +0300

    123

--- Practice-2-4.py
+++ Practice-2-4.py
@@ -26,6 +26,34 @@ class Stack:
 
 
 def trains(s):
-    pass
+    st1 = Stack()
+    st2 = Stack()
+    st_z = Stack()
+
+    for i in s.split(" "):
+        print(i)
+        st1.push(int(i))
+    st_z.push(st1.pop())
+    while True:
+        
+        # print(f'a = {st_z.peek()}')
+        if st2.is_empty():
+            if st_z.peek() == 1:
+                st2.push(st_z.pop())
+        else:
+            if st_z.peek() == (st2.peek() + 1):
+                st2.push(st_z.pop())
+        # print(st2.peek())
+        if st1.is_empty():
+            if st_z.is_empty():
+                return "Выпонено"
+            elif st_z.peek() != (st2.peek() + 1):
+                return "Невозможно"
+        else: 
+            st_z.push(st1.pop())
+        print(f'1 = {st1.peek()}, 2 = {st2.peek()}, z = {st_z.peek()}')
+
+    # return "Невозможно"
+
 
 print(trains(input('Wagons :')))
